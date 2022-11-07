b7ffe80827a837b5a5205791b2498193e9a86c39
jaykhan-dev <jaykhan.sound@gmail.com>
2022-11-07 14:19:26 -0500

    spint react first

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Keyboard3D from "./components/spline/Keyboard3D";
+import ThreeDText from "./components/spline/ThreeDText";
+
+function App() {
+  return (
+    <div className="App grid place-items-center h-screen">
+      {/* <Keyboard3D /> */}
+      <ThreeDText />
+    </div>
+  );
+}
+
+export default App;
